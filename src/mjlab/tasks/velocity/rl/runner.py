68dfb7c9c788bacb2d4eec4e664f95f77660b543
Larry Hui <larryhui7@gmail.com>
2025-12-17 12:37:33 -0800

    Refactor ONNX export logic for WandB integration
    changed runner.py

--- src/mjlab/tasks/velocity/rl/runner.py
+++ src/mjlab/tasks/velocity/rl/runner.py
@@ -9,30 +9,70 @@ from mjlab.tasks.velocity.rl.exporter import (
   export_velocity_policy_as_onnx,
 )
 
-
 class VelocityOnPolicyRunner(OnPolicyRunner):
   env: RslRlVecEnvWrapper
 
   def save(self, path: str, infos=None):
     """Save the model and training information."""
     super().save(path, infos)
-    if self.logger_type in ["wandb"]:
-      policy_path = path.split("model")[0]
-      filename = os.path.basename(os.path.dirname(policy_path)) + ".onnx"
-      if self.alg.policy.actor_obs_normalization:
-        normalizer = self.alg.policy.actor_obs_normalizer
-      else:
-        normalizer = None
-      export_velocity_policy_as_onnx(
-        self.alg.policy,
-        normalizer=normalizer,
-        path=policy_path,
-        filename=filename,
-      )
-      attach_onnx_metadata(
-        self.env.unwrapped,
-        wandb.run.name,  # type: ignore
-        path=policy_path,
-        filename=filename,
-      )
-      wandb.save(policy_path + filename, base_path=os.path.dirname(policy_path))
+
+    # Only export ONNX + log if WandB is actually active
+    if self.logger is None:
+      return
+
+    # Check logger type robustly
+    if self.logger.__class__.__name__.lower().startswith("wandb"):
+        policy_path = path.split("model")[0]
+        filename = os.path.basename(os.path.dirname(policy_path)) + ".onnx"
+
+        # Optional observation normalizer
+        if self.alg.policy.actor_obs_normalization:
+            normalizer = self.alg.policy.actor_obs_normalizer
+        else:
+            normalizer = None
+
+        export_velocity_policy_as_onnx(
+            self.alg.policy,
+            normalizer=normalizer,
+            path=policy_path,
+             filename=filename,
+        )
+
+        attach_onnx_metadata(
+            self.env.unwrapped,
+            wandb.run.name,  # type: ignore
+            path=policy_path,
+            filename=filename,
+        )
+
+        wandb.save(
+            policy_path + filename,
+            base_path=os.path.dirname(policy_path),
+        )
+
+# class VelocityOnPolicyRunner(OnPolicyRunner):
+#   env: RslRlVecEnvWrapper
+
+#   def save(self, path: str, infos=None):
+#     """Save the model and training information."""
+#     super().save(path, infos)
+#     if self.logger_type in ["wandb"]:
+#       policy_path = path.split("model")[0]
+#       filename = os.path.basename(os.path.dirname(policy_path)) + ".onnx"
+#       if self.alg.policy.actor_obs_normalization:
+#         normalizer = self.alg.policy.actor_obs_normalizer
+#       else:
+#         normalizer = None
+#       export_velocity_policy_as_onnx(
+#         self.alg.policy,
+#         normalizer=normalizer,
+#         path=policy_path,
+#         filename=filename,
+#       )
+#       attach_onnx_metadata(
+#         self.env.unwrapped,
+#         wandb.run.name,  # type: ignore
+#         path=policy_path,
+#         filename=filename,
+#       )
+#       wandb.save(policy_path + filename, base_path=os.path.dirname(policy_path))
